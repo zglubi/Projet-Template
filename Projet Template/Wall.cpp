1472b3d7c5566988115a18525244f3a4375f5a0b
elovisa <elovisa@gaming.tech>
2025-01-30 17:23:00 +0100

    stairs texture on map working

--- Projet Template/Wall.cpp
+++ Projet Template/Wall.cpp
@@ -2,10 +2,53 @@
 
 Wall::Wall(float x, float y, char t) : Entity(x, y), type(t)
 {
-	texture.loadFromFile("assets/map_assets/props.png");
-	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(288, 306, 32, 46));
-	sprite.setPosition(Vector2f(x, y - 14));
+	switch (type)
+	{
+	case 'R':
+		texture.loadFromFile("assets/map_assets/props.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(288, 306, 32, 46));
+		sprite.setPosition(Vector2f(x, y - 14));
+		break;
+	case 'S':
+		texture.loadFromFile("assets/map_assets/struct.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(32, 160, 64, 96));
+		sprite.setPosition(Vector2f(x, y));
+		break;
+
+	case 's':
+		texture.loadFromFile("assets/map_assets/struct.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(32, 32, 64, 96));
+		sprite.setPosition(Vector2f(x, y));
+		break;
+	case 'E':
+		texture.loadFromFile("assets/map_assets/struct.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(128, 160, 64, 96));
+		sprite.setPosition(Vector2f(x, y));
+		break;
+	case 'e':
+		texture.loadFromFile("assets/map_assets/struct.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(128, 32, 64, 96));
+		sprite.setPosition(Vector2f(x, y));
+		break;
+	case 'Q':
+		texture.loadFromFile("assets/map_assets/struct.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(224, 160, 64, 96));
+		sprite.setPosition(Vector2f(x, y));
+		break;
+	case 'q':	
+		texture.loadFromFile("assets/map_assets/struct.png");
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(224, 32, 64, 96));
+		sprite.setPosition(Vector2f(x, y));
+		break;
+	}
+
 }
 
 Wall::~Wall()
