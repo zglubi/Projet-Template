caeef7fc44d6f5424e913f6fc37126ffcc65c708
TeoXpok <goncalves.teo04@gmail.com>
2025-01-30 14:39:51 +0100

    Entity h,cpp + PLayerH

---
+++ Projet Template/Entity.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef ENTITY_H
+#define ENtity_H
+
+
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+
+
+
+class Entity
+{
+public:
+	
+	Sprite sprite;
+	Texture texture;
+
+	float x;
+	float y;
+
+
+	Entity(float x, float y);
+	virtual void update(RenderWindow& window, Time deltatime) = 0;
+	virtual void draw(RenderWindow& window) = 0;
+
+};
+
+
+
+#endif // !ENTITY_H
+
