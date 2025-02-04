bf63445b829aba5c728be1aec5b4e6665aab562b
tbouaifel <tbouaifel@GCFRPA049>
2025-02-04 17:11:00 +0100

    sdsd

--- Projet Template/Boss.h
+++ Projet Template/Boss.h
@@ -6,7 +6,17 @@
 #include <SFML/Graphics.hpp>
 
 class Boss : public Enemy {
+private:
+    float initialSpeed;
+    int moveX;
+    int moveY;
 
+public:
+    Boss(Vector2f startPosition, float initialSpeed);
+
+    void collisionPlayer(shared_ptr<Player>& player) override;
+    void update(RenderWindow& window, float deltatime, View& view) override;
+    void draw(RenderWindow& window) override;
 };
 
 #endif // BOSS_H
