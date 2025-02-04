f20c26a7d6fd4cbc467d4b9439c1341fc0b17764
tbouaifel <tbouaifel@GCFRPA049>
2025-02-04 18:01:05 +0100

    Ajout boss (pas encore fini)

--- Projet Template/Boss.cpp
+++ Projet Template/Boss.cpp
@@ -1,1 +1,61 @@
 #include "Boss.h"
+
+Boss::Boss(Vector2f startPosition, float initialSpeed)
+    : Enemy(startPosition.x, startPosition.y, initialSpeed), initialSpeed(initialSpeed), moveX(0), moveY(0), fireCooldown(1.3f), fireTimer(0.0f) 
+{
+	if (!texture.loadFromFile("assets/Enemies/Boss/Boss.png")) {
+		//cout << "Failed to load enemy texture!" << endl;
+	}
+
+	sprite.setTextureRect(IntRect(0, 0, 25, 25));
+	sprite.setTexture(texture);
+	sprite.setScale(2, 2);
+	sprite.setOrigin(sprite.getGlobalBounds().width / 2.f, sprite.getGlobalBounds().height / 2.f);
+	sprite.setPosition(startPosition);
+
+	moveX = 0;
+	moveY = 0;
+}
+
+void Boss::collisionPlayer(shared_ptr<Player>& player) 
+{
+	if (sprite.getGlobalBounds().intersects(player->getSprite().getGlobalBounds()))
+	{
+		player->diminishHp(1);
+		hp -= 1;
+	}
+	for (auto& projectile : projectiles)
+	{
+		projectile->collisionPlayer(player);
+	}
+}
+
+void Boss::update(RenderWindow& window, float deltatime, View& view) 
+{
+	Vector2f playerPos = view.getCenter();
+	Vector2f enemyPos = sprite.getPosition();
+	float distanceX = abs(playerPos.x - enemyPos.x);
+	float distanceY = abs(playerPos.y - enemyPos.y);
+	float distance = sqrt(distanceX * distanceX + distanceY * distanceY);
+
+
+}
+
+void Boss::draw(RenderWindow& window) 
+{
+	window.draw(sprite);
+
+	// Draw projectiles
+	for (const auto& projectile : projectiles)
+	{
+		projectile->draw(window);
+	}
+}
+
+void Boss::fireProjectile(Vector2f direction) 
+{
+	Vector2f startPosition = sprite.getPosition();
+	float speed = 200.0f;
+	float damage = 15.0f;
+	projectiles.emplace_back(make_unique<Projectile>(projectileTexture, startPosition, direction, speed, damage, 4, 24, 24));
+}
--- Projet Template/Boss.h
+++ Projet Template/Boss.h
@@ -3,20 +3,26 @@
 
 #include "Enemy.h"
 #include "Player.h"
+#include "Projectile.h"
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 class Boss : public Enemy {
 private:
     float initialSpeed;
     int moveX;
     int moveY;
+    vector<unique_ptr<Projectile>> projectiles;
+    Texture projectileTexture;
+    float fireCooldown;
+    float fireTimer;
 
 public:
     Boss(Vector2f startPosition, float initialSpeed);
-
     void collisionPlayer(shared_ptr<Player>& player) override;
     void update(RenderWindow& window, float deltatime, View& view) override;
     void draw(RenderWindow& window) override;
+    void fireProjectile(Vector2f direction);
 };
 
 #endif // BOSS_H
